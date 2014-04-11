5072dff199ee5651fd1f43b1762b8575cbcef9a0
thibthibaut <thibaut@europe.com>
2014-04-11 12:19:49 +0200

    adding a line

--- SuperToubib/src/MainClass.java
+++ SuperToubib/src/MainClass.java
@@ -15,6 +15,9 @@ public class MainClass {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Bienvenue sur SUPERTOUBIB!");
+        
     }
     
 }
